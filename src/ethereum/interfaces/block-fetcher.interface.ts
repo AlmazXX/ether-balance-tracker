d24574a4a95b6924c15a4e7fb98f0e23be4196b1
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2025-03-09 22:43:43 +0600

    feat: added rate limit enforcement

--- src/ethereum/interfaces/block-fetcher.interface.ts
+++ src/ethereum/interfaces/block-fetcher.interface.ts
@@ -3,13 +3,42 @@ import { Transform, TransformCallback } from 'stream';
 
 export class BlockFetcher extends Transform {
   private retryCount = 3;
+  private concurrentRequests = 3;
   private activeRequests = 0;
+  private lastRequestTime = 0;
+  private currentRateLimit = 200;
+  private requestTimes: number[] = [];
   private queue: Array<{ blockNum: number; callback: TransformCallback }> = [];
-  constructor(
-    private readonly httpService: HttpService,
-    private concurrentRequests: number,
-  ) {
+
+  constructor(private readonly httpService: HttpService) {
     super({ objectMode: true });
+  }
+
+  private async enforceRateLimit() {
+    const timeSinceLastRequest = Date.now() - this.lastRequestTime;
+
+    if (timeSinceLastRequest < this.currentRateLimit) {
+      await new Promise((res) =>
+        setTimeout(res, this.currentRateLimit - timeSinceLastRequest),
+      );
+    }
+
+    this.lastRequestTime = Date.now();
+  }
+
+  private updateRateLimit() {
+    if (this.requestTimes.length > 0) {
+      this.requestTimes.shift();
+    }
+
+    const avgTime =
+      this.requestTimes.reduce((a, b) => a + b, 0) / this.requestTimes.length;
+
+    if (avgTime > this.currentRateLimit * 0.8) {
+      this.currentRateLimit = Math.min(this.currentRateLimit * 1.2, 500);
+    } else if (avgTime < this.currentRateLimit * 0.5) {
+      this.concurrentRequests = Math.min(10, this.concurrentRequests + 1);
+    }
   }
 
   private async processQueue() {
@@ -28,10 +57,13 @@ export class BlockFetcher extends Transform {
   private async fetchBlock(blockNum: number, callback: TransformCallback) {
     this.activeRequests++;
     let attempts = 0;
+    const startTime = Date.now();
 
     while (attempts < this.retryCount) {
       try {
+        await this.enforceRateLimit();
         const hexBlockNum = `0x${blockNum.toString(16)}`;
+
         const { data } = await this.httpService.axiosRef.get('', {
           params: {
             module: 'proxy',
@@ -44,6 +76,10 @@ export class BlockFetcher extends Transform {
         if (data.error) {
           throw new Error(`Etherscan API error: ${data.error}`);
         }
+
+        const requestTime = Date.now() - startTime;
+        this.requestTimes.push(requestTime);
+        this.updateRateLimit();
 
         this.push(data.result.transactions || []);
         this.activeRequests--;
